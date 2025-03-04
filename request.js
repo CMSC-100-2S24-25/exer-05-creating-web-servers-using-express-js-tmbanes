645d086f92672313420af8f8df8bc5d8870e1f03
tmbanes <tmbanes@up.edu.ph>
2025-03-04 23:47:05 +0800

    feat: updated /find-by-author get method

--- request.js
+++ request.js
@@ -1,13 +1,22 @@
 import needle from 'needle';
 
-needle.get('http://localhost:3000/', (err, res) => {
-    console.log(res.body); // prints the body of the response message. In this case, “Hello”
+needle.get('http://localhost:3000/find-by-isbn-author', (err, res) => {
+    console.log(res.body);
+});
+
+needle.get('http://localhost:3000/find-by-author', (err, res) => {
+    console.log(res.body);
 });
 
 needle.post(
     'http://localhost:3000/add-book',
-    {},
+    {
+        bookName:"",
+        isbn: "",
+        author: "",
+        yearPublish: "",
+    },
     (err, res) => {
-        console.log(res.body) // prints the server’s response “Received a POST request.”
+        console.log(res.body)
     }
 );
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.get('/find-by-isbn-author', (req, res) => {
-    const data = readFileSync(books.txt, "utf-8");  // read books.txt file
+    const data = readFileSync("books.txt", "utf-8");  // read books.txt file
     const books = data.split('\n');                  // creates an array of books in text file
     var bookFound = false;
     var bookSearch = [];
@@ -24,7 +24,7 @@ app.get('/find-by-isbn-author', (req, res) => {
         var bookInfo = book.split(','); //creates array for each info in book
 
         if(bookInfo[1] === req.query.isbn && bookInfo[2] === req.query.author){
-            bookSearch = book;
+            bookSearch = bookInfo; //passes the bookInfo to bookSearch
             bookFound = true;
             break;
         }
@@ -32,36 +32,56 @@ app.get('/find-by-isbn-author', (req, res) => {
 
     if(bookFound){
         res.send(
-            'Book: ' + bookSearch[0] + '\n'+
-            'Author: ' + bookSearch[1] + '\n'+
-            'ISBN: ' + bookSearch[2] + '\n' +
-            'Year Published: ' + bookSearch[3] + '\n'
-        );
+            'Book: ' + bookSearch[0] + '<br>'+
+            'ISBN: ' + bookSearch[1] + '<br>'+
+            'Author: ' + bookSearch[2] + '<br>' +
+            'Year Published: ' + bookSearch[3] + '<br>'
+        );// prints book information
     }else{
         res.send("Book not found.")
     }
 });
 
 app.get('/find-by-author', (req, res) => {
-    res.send(
-        'Book: ' + req.query.book + '\n'+
-        'Author: ' + req.query.author + '\n'+
-        'ISBN: ' + req.query.isbn + '\n' +
-        'Year Published: ' + req.query.yearPublished + '\n'
-    );
+    const data = readFileSync("books.txt", "utf-8");  // read books.txt file
+    const books = data.split('\n');                  // creates an array of books in text file
+    var foundBooks = [];
+
+    for(let book of books){
+        var bookInfo = book.split(',');             //creates array for each info in book
+
+        if(bookInfo[2] === req.query.author){
+            foundBooks.push(book);
+        }
+    }
+
+    if(foundBooks.length > 0){
+        var booksByAuthor = [];
+        for(let book of foundBooks){
+            var bookInfo = book.split(',');         //creates array for each info in book
+            booksByAuthor.push(bookInfo[0])         //pushes name of book in booksByAuthor  array
+        }
+        res.send(
+            'Author: ' + req.query.author + '<br>' + booksByAuthor // prints every book in author
+        );
+    }else{
+        res.send("Author does not have books.")
+    }
 });
 
 
 app.post('/add-book', (req, res) => {
     var bookAdded = false;
+
     bookName = req.body.bookName;
     isbn = req.body.isbn;
     author = req.body.author;
     yearPublished = req.body.yearPublished;
+    // this will set all input to body
 
     if(!isEmpty(bookName) || !isEmpty(isbn) || !isEmpty(author) || !isEmpty(yearPublished)){
         var book = bookName + "," + isbn + "," + author + "," + yearPublished + "\n";
-        appendFileSync(books.txt, "book");
+        appendFileSync(books.txt, "book"); //add book info to file
         bookAdded = true;
         console.log("success: ", bookAdded);
     }else{
